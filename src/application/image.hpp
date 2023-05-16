3efe6bde51a59fec9c0010053c394185344cc04b
Jan Juracka <janjuracka861@seznam.cz>
2023-05-16 11:02:19 +0200

    ADD: default ndImage template

--- src/application/image.hpp
+++ src/application/image.hpp
@@ -2,7 +2,9 @@
 #include "meta_types.hpp"
 #include <cassert>
 #include <cstddef>
+#include <initializer_list>
 #include <memory>
+#include <numeric>
 #include <tuple>
 #include <type_traits>
 #include <vector>
@@ -31,17 +33,20 @@ using type_list = std::tuple<uint8_t,
                              std::array<uint8_t, 4>>;
 
 template <types type>
-using _get_img_type =
-    std::tuple_element_t<static_cast<std::size_t>(type), type_list>;
+using enum_to_type = std::tuple_element_t<static_cast<int>(type), type_list>;
 
-using GRAY8 = _get_img_type<types::GRAY8>;
-using GRAY16 = _get_img_type<types::GRAY16>;
-using GRAY32 = _get_img_type<types::GRAY32>;
-using GRAY64 = _get_img_type<types::GRAY64>;
-using FLOAT = _get_img_type<types::FLOAT>;
-using DOUBLE = _get_img_type<types::DOUBLE>;
-using RGB8 = _get_img_type<types::RGB8>;
-using RGBA8 = _get_img_type<types::RGBA8>;
+template <typename T>
+constexpr types type_to_enum =
+    static_cast<types>(int(mt::traits::tuple_type_idx_v<T, type_list>));
+
+using GRAY8 = enum_to_type<types::GRAY8>;
+using GRAY16 = enum_to_type<types::GRAY16>;
+using GRAY32 = enum_to_type<types::GRAY32>;
+using GRAY64 = enum_to_type<types::GRAY64>;
+using FLOAT = enum_to_type<types::FLOAT>;
+using DOUBLE = enum_to_type<types::DOUBLE>;
+using RGB8 = enum_to_type<types::RGB8>;
+using RGBA8 = enum_to_type<types::RGBA8>;
 
 template <typename T>
 concept ImgType = requires { mt::concepts::TupleType<T, type_list>; };
@@ -54,6 +59,13 @@ template <typename T>
 class ndImage;
 
 class ndImageBase {
+  protected:
+	ndImageBase(std::initializer_list<std::size_t> dims,
+	            std::size_t data_size,
+	            types type)
+	    : _data(std::make_shared<std::vector<std::byte>>(data_size)),
+	      _dims(dims), _type(type) {}
+
   public:
 	ndImageBase() = delete;
 	ndImageBase(const ndImageBase&) = default;
@@ -65,7 +77,7 @@ class ndImageBase {
 
 	template <ImgType T>
 	ndImage<T> as_typed() {
-		assert((static_cast<int>(_type) == mt::traits::tuple_type_idx_v<T, type_list>));
+		assert(_type == type_to_enum<T>);
 		return ndImage<T>(*this);
 	}
 
@@ -73,6 +85,20 @@ class ndImageBase {
 	std::shared_ptr<std::vector<std::byte>> _data;
 	std::vector<std::size_t> _dims;
 	types _type;
+};
+
+template <typename T>
+class ndImage : public ndImageBase {
+  public:
+	template <typename... dims_t>
+	    requires std::conjunction_v<std::is_same<std::size_t, dims_t>...>
+	ndImage(dims_t... raw_dims) {
+		std::initializer_list<std::size_t> dims = {raw_dims...};
+		std::size_t data_size =
+		    std::reduce(dims.begin(), dims.end(), 1, std::multiplies{}) *
+		    sizeof(T);
+		ndImageBase(dims, data_size, type_to_enum<T>);
+	}
 };
 
 } // namespace img
