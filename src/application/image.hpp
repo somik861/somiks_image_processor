78fc76a3751012db9e6c96522881c0dc7e3d478c
Jan Juracka <janjuracka861@seznam.cz>
2023-05-17 14:01:31 +0200

    ADD: Rest of Image implementations ADD: Some comments

--- src/application/image.hpp
+++ src/application/image.hpp
@@ -1,4 +1,12 @@
 #pragma once
+
+/**
+ * This file provides a basic wrapper for passing images throught the
+ * application.
+ *
+ * All code is placed inside **img** namespace.
+ */
+
 #include "meta_types.hpp"
 #include <algorithm>
 #include <cassert>
@@ -14,6 +22,9 @@
 
 namespace img {
 
+/**
+ * Supported image element types enumeration
+ */
 enum class types : int {
 	GRAY8,
 	GRAY16,
@@ -26,6 +37,9 @@ enum class types : int {
 };
 
 namespace type_aliases {
+/**
+ * Type list of c++-types corresponding to the enumeration above
+ */
 using type_list = std::tuple<uint8_t,
                              uint16_t,
                              uint32_t,
@@ -35,13 +49,23 @@ using type_list = std::tuple<uint8_t,
                              std::array<uint8_t, 3>,
                              std::array<uint8_t, 4>>;
 
+/**
+ * Convenience function for convering enum type to c++-type
+ */
 template <types type>
 using enum_to_type = std::tuple_element_t<static_cast<int>(type), type_list>;
 
+/**
+ * Convenience function for convering c++-type to enum type
+ */
 template <typename T>
 constexpr types type_to_enum =
     static_cast<types>(int(mt::traits::tuple_type_idx_v<T, type_list>));
 
+/**
+ * Once again, now with type aliases to make the image manipulation as
+ * convenient as possible
+ */
 using GRAY8 = enum_to_type<types::GRAY8>;
 using GRAY16 = enum_to_type<types::GRAY16>;
 using GRAY32 = enum_to_type<types::GRAY32>;
@@ -51,26 +75,48 @@ using DOUBLE = enum_to_type<types::DOUBLE>;
 using RGB8 = enum_to_type<types::RGB8>;
 using RGBA8 = enum_to_type<types::RGBA8>;
 
+/**
+ * Concept to prevent instatiation of template swith unsupported type
+ */
 template <typename T>
 concept ImgType = requires { mt::concepts::TupleType<T, type_list>; };
 
 } // namespace type_aliases
 
+/**
+ * Make type aliases visible in the **img** namespace (once again for
+ * convenience :D)
+ */
 using namespace type_aliases;
 
 template <typename T>
 class ndImage;
 
+/**
+ * Base of all images.
+ * It can be looked at as an type-erased wrapper for easier passing aroung.
+ * It cannot be instatiated directly (there is not reasonable constructor),
+ * to create an image, use **ndImage** below.
+ *
+ * Images can be passed by value, as it base contains all the attributes ndImage
+ * needs.
+ *
+ * Copies via assignment operators are shallow. For deepcopy, use **copy**;
+ *
+ */
 class ndImageBase {
+  private:
+	ndImageBase() = default;
+
   protected:
-	ndImageBase(std::initializer_list<std::size_t> dims,
+	ndImageBase(std::span<const std::size_t> dims,
 	            std::size_t elem_size,
 	            types type)
 	    : _data(std::make_shared<std::vector<std::byte>>(
 	          std::reduce(
 	              dims.begin(), dims.end(), std::size_t(1), std::multiplies{}) *
 	          elem_size)),
-	      _dims(dims), _type(type) {}
+	      _dims(dims.begin(), dims.end()), _type(type) {}
 
   public:
 	ndImageBase(const ndImageBase&) = default;
@@ -78,12 +124,36 @@ class ndImageBase {
 	ndImageBase& operator=(const ndImageBase&) = default;
 	ndImageBase& operator=(ndImageBase&&) = default;
 
+	/**
+	 * Get dimensions of image
+	 */
 	const std::vector<std::size_t>& dims() const { return _dims; }
 
+	/**
+	 * Original type of the image
+	 */
+	types type() const { return _type; }
+
+	/**
+	 * Return typed version of this image, that is: ndImage<T>.
+	 * The image type must correspond to the original one.
+	 */
 	template <ImgType T>
 	ndImage<T> as_typed() {
 		assert(_type == type_to_enum<T>);
 		return ndImage<T>(*this);
+	}
+
+	/**
+	 * Deep copy of the image
+	 */
+	ndImageBase copy() const {
+		ndImageBase cpy;
+		cpy._type = _type;
+		cpy._dims = _dims;
+		cpy._data = std::make_shared<std::vector<std::byte>>(_data->begin(),
+		                                                     _data->end());
+		return cpy;
 	}
 
   protected:
@@ -105,29 +175,30 @@ class ndImage : public ndImageBase {
 	ndImage(std::initializer_list<std::size_t> il)
 	    : ndImageBase(il, sizeof(T), type_to_enum<T>) {}
 
+	std::span<T> span() {
+		return reinterpret_cast<std::span<T>>(std::span(*_data));
+	}
+
+	std::span<const T> span() const {
+		return reinterpret_cast<std::span<const T>>(std::span(*_data));
+	}
+
+	// ======== INDEXED ACCESS ==========
+
+	T& operator()(std::size_t idx) {
+		assert(idx < span().size());
+		return span()[idx];
+	}
+
+	const T& operator()(std::size_t idx) const {
+		assert(idx < span().size());
+		return span()[idx];
+	}
+
 	template <typename... dims_t>
 	    requires std::conjunction_v<std::is_same<int, dims_t>...>
 	T& operator()(dims_t... coords) {
 		return (*this)({std::size_t(coords)...});
-	}
-
-	T& operator()(std::initializer_list<std::size_t> coords) {
-		assert(coords.size() == _dims.size());
-
-		std::size_t flat_idx = 0;
-
-		auto il_it = coords.begin();
-
-		for (int i = int(_dims.size()); i > 0; --i) {
-			assert(*il_it < _dims[i]);
-			flat_idx += *il_it;
-			flat_idx *= _dims[i];
-			++il_it;
-		}
-		assert(*il_it < _dims[0]);
-		flat_idx += *il_it;
-
-		return reinterpret_cast<std::span<T>>(std::span(*_data))[flat_idx];
 	}
 
 	template <typename... dims_t>
@@ -136,12 +207,40 @@ class ndImage : public ndImageBase {
 		return (*this)({std::size_t(coords)...});
 	}
 
+	T& operator()(std::initializer_list<std::size_t> coords) {
+		return span()[_get_flat_idx(coords)];
+	}
 	const T& operator()(std::initializer_list<std::size_t> coords) const {
-		assert(coords.size() == _dims.size());
+		return span()[_get_flat_idx(coords)];
+	}
+
+	// ======== ITERATOR ACCESS ==========
+	auto begin() { return span().begin(); }
+	auto begin() const { return span().begin(); }
+	auto cbegin() const { return begin(); }
+	auto rbegin() { return span().rbegin(); }
+	auto rbegin() const { return span().rbegin(); }
+	auto crbegin() const { return rbegin(); }
+
+	auto end() { return span.end(); }
+	auto end() const { return span.end(); }
+	auto cend() const { return end(); }
+	auto rend() { return span.rend(); }
+	auto rend() const { return span.rend(); }
+	auto crend() const { rend(); }
+
+  private:
+	/**
+	 * Calculate flat index of the element.
+	 * The index is calculated in such a way, that increasing first coordinate
+	 * results in coalesced access.
+	 */
+	std::size_t _get_flat_idx(std::initializer_list<std::size_t> il) const {
+		assert(il.size() == _dims.size());
 
 		std::size_t flat_idx = 0;
 
-		auto il_it = coords.begin();
+		auto il_it = il.begin();
 
 		for (int i = int(_dims.size()); i > 0; --i) {
 			assert(*il_it < _dims[i]);
@@ -152,8 +251,7 @@ class ndImage : public ndImageBase {
 		assert(*il_it < _dims[0]);
 		flat_idx += *il_it;
 
-		return reinterpret_cast<std::span<const T>>(
-		    std::span(*_data))[flat_idx];
+		return flat_idx;
 	}
 };
 
