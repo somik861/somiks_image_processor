dd93882a71312c90da2af12d00b26372e06b861f
Jan Juracka <janjuracka861@seznam.cz>
2023-05-19 07:38:40 +0200

    ADD: Basic raw matching test

--- tests/extension_matcher.cpp
+++ tests/extension_matcher.cpp
@@ -1,3 +1,18 @@
+#include "../src/application/extension_matcher.hpp"
 #include "common.hpp"
 
-TEST_CASE("ExtensionMatcher") { REQUIRE(true == true); }
+TEST_CASE("ExtensionMatcher") {
+	ExtensionMatcher ext_match;
+
+	SECTION("Raw matching") {
+		ext_match.register_extension("tiff", "tif");
+		ext_match.register_extension("jpeg", "jpg");
+
+		std::vector<std::string> formats =
+		    ext_match.sorted_formats_by_priority("img.jpg");
+
+		REQUIRE(formats.size() == 2);
+		REQUIRE(formats[0] == "jpg");
+		REQUIRE(formats[1] == "tiff");
+	}
+}
