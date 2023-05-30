52cd599773ee62f7829234c4860efbfb00b341c0
Jan Juracka <janjuracka861@seznam.cz>
2023-05-30 17:00:03 +0200

    ADD: Atributes and interface improvements

--- src/application/managers/format_manager.cpp
+++ src/application/managers/format_manager.cpp
@@ -1,1 +1,18 @@
 #include "format_manager.hpp"
+
+namespace fs = std::filesystem;
+
+namespace ssimp {
+FormatManager::FormatManager() {}
+
+std::vector<img::LocalizedImage>
+FormatManager::load_image(const fs::path& path) const {
+	return {};
+}
+
+void FormatManager::save_image(const fs::path& directory,
+                               const img::LocalizedImage& image,
+                               const std::string& format,
+                               const OptionsManager::options_t& options) const {
+}
+} // namespace ssimp
--- src/application/managers/format_manager.hpp
+++ src/application/managers/format_manager.hpp
@@ -3,18 +3,47 @@
 #include "../nd_image.hpp"
 #include "options_manager.hpp"
 #include <filesystem>
+#include <functional>
+#include <optional>
 #include <tuple>
+#include <unordered_map>
+#include <vector>
 
 namespace ssimp {
 class FormatManager {
   private:
-	using registered_types = std::tuple<>;
+	using _registered_types = std::tuple<>;
 
   public:
-	void initialize();
-	img::LocalizedImage load_image(const std::filesystem::path& path) const;
+	/**
+	 * Modified constructor for format manager initialization.
+	 */
+	FormatManager();
+
+	/**
+	 * Detect automatically format and try to open the image.
+	 */
+	std::vector<img::LocalizedImage>
+	load_image(const std::filesystem::path& path) const;
+
+	/**
+	 * Save **image** in **directory** using using default extension of a
+	 * format.
+	 */
 	void save_image(const std::filesystem::path& directory,
 	                const img::LocalizedImage& image,
+	                const std::string& format,
 	                const OptionsManager::options_t& options) const;
+
+  private:
+	std::unordered_map<std::string,
+	                   std::function<std::vector<img::LocalizedImage>(
+	                       const std::filesystem::path&)>>
+	    _image_loaders;
+	std::unordered_map<std::string,
+	                   std::function<void(const img::ndImageBase&,
+	                                      const std::filesystem::path&,
+	                                      const OptionsManager::options_t&)>>
+	    _image_savers;
 };
 } // namespace ssimp
