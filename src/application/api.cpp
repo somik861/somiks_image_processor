5feba888792c4c2aabc2f5015261979dbd3e1c54
Jan Juracka <janjuracka861@seznam.cz>
2023-06-01 11:45:28 +0200

    ADD: API initialization

--- src/application/api.cpp
+++ src/application/api.cpp
@@ -1,3 +1,26 @@
 #include "api.hpp"
+#include "managers/config_manager.hpp"
+#include "managers/extension_manager.hpp"
+#include "managers/format_manager.hpp"
+#include "managers/options_manager.hpp"
 
-namespace ssimp {}
+namespace ssimp {
+API::API()
+    : _config_manager(std::make_unique<ConfigManager>()),
+      _extension_manager(std::make_unique<ExtensionManager>()),
+      _format_manager(std::make_unique<FormatManager>()),
+      _options_manager(std::make_unique<OptionsManager>()) {
+
+	for (const std::string& format : _format_manager->registered_formats()) {
+		auto config = _config_manager->load_format(format).get_object();
+
+		_extension_manager->load_from_json(
+		    format, config.at("extensions").get_array(),
+		    config.at("output_extension").get_string());
+
+		_options_manager->load_from_json(format,
+		                                 config.at("options").get_array());
+	}
+}
+
+} // namespace ssimp
