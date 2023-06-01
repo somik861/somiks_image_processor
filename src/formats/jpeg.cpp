c744eb016d0273ef3b8be492fffa884fc78a3001
Jan Juracka <janjuracka861@seznam.cz>
2023-06-01 17:34:07 +0200

    ADD: JPEG empty files

---
+++ src/formats/jpeg.cpp
@@ -0,0 +1,24 @@
+#include "jpeg.hpp"
+
+#include "common.hpp"
+
+namespace ssimp::formats {
+/* static */ std::vector<img::LocalizedImage>
+JPEG::load_image(const fs::path& path) {
+
+	return {{img::ndImage<img::GRAY8>(2, 2), path.filename()}};
+}
+
+/* static */
+std::optional<ImageProperties> JPEG::get_information(const fs::path& path) {
+	return ImageProperties{name, {}, {}};
+}
+
+template <typename T>
+    requires mt::traits::is_any_of_tuple_v<T, JPEG::supported_types>
+/* static */ void JPEG::save_image(const img::ndImage<T>& img,
+                                   const fs::path& path,
+                                   const option_types::options_t& options) {}
+
+INSTANTIATE_SAVE_TEMPLATE(JPEG, img::GRAY8);
+} // namespace ssimp::formats
