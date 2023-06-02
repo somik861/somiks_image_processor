b522ded494b935da1ae61aa7e6fefc52f6f97e09
Jan Juracka <janjuracka861@seznam.cz>
2023-06-02 08:03:48 +0200

    ADD: SSIMP exceptions

--- src/application/util_types.hpp
+++ src/application/util_types.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <variant>
 #include <vector>
 
 namespace ssimp {
+namespace exceptions {
+class Exception : public std::exception {
+  public:
+	Exception(std::string what_arg) : _what(std::move(what_arg)) {}
+	virtual const char* what() const noexcept { return _what.data(); }
+
+  private:
+	std::string _what;
+};
+
+class IOError : public Exception {
+  public:
+	using Exception::Exception;
+};
+
+class Unsupported : public Exception {
+  public:
+	using Exception::Exception;
+};
+
+} // namespace exceptions
 namespace option_types {
 /**
  * Variant of possible option values
