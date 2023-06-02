62fdef9820000672d54635e7fb5772c07293086f
Jan Juracka <janjuracka861@seznam.cz>
2023-06-02 09:11:32 +0200

    WIP: information get

--- src/formats/jpeg.cpp
+++ src/formats/jpeg.cpp
@@ -1,6 +1,34 @@
 #include "jpeg.hpp"
 #include "common.hpp"
+#include <optional>
 #include <turbojpeg.h>
+
+namespace {
+class JpegMeta {
+  public:
+	std::size_t width;
+	std::size_t height;
+	int jpegSubsamp;
+	int jpegColorspace;
+};
+
+std::optional<JpegMeta> jpeg_info(tjhandle decompressor,
+                                  std::span<const std::byte> bytes) {
+	int _width, _height;
+	JpegMeta out;
+
+	if (tjDecompressHeader3(
+	        decompressor, reinterpret_cast<const unsigned char*>(bytes.data()),
+	        bytes.size(), &_width, &_height, &out.jpegSubsamp,
+	        &out.jpegColorspace))
+		return {};
+
+	out.height = std::size_t(_height);
+	out.width = std::size_t(_width);
+	return out;
+}
+
+} // namespace
 
 namespace ssimp::formats {
 /* static */ std::vector<img::LocalizedImage>
@@ -13,15 +41,17 @@ JPEG::load_image(const fs::path& path) {
 /* static */
 std::optional<ImageProperties> JPEG::get_information(const fs::path& path) {
 	tjhandle decompressor = tjInitDecompress();
-
-	int width, height, jpegSubsamp, jpegColorspace;
 	auto bytes = details::read_file(path);
 
-	tjDecompressHeader3(
-	    decompressor, reinterpret_cast<const unsigned char*>(bytes.data()),
-	    bytes.size(), &width, &height, &jpegSubsamp, &jpegColorspace);
+	std::optional<JpegMeta> meta_data = jpeg_info(decompressor, bytes);
+	if (!meta_data)
+		return {};
 
-	return ImageProperties(name, {std::size_t(width), std::size_t(height)}, {});
+	ImageProperties out(name, {meta_data->width, meta_data->height}, {});
+	out.others["Chroma Subsampling"] = "";
+	out.others["Colorspace"] = "";
+
+	return out;
 }
 
 template <typename T>
