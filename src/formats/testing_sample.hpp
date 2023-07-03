f5803b583903cef2201edd6d8c64aa5747eba195
Jan Juracka <janjuracka861@seznam.cz>
2023-07-03 15:50:18 +0200

    FIX: formatting

--- src/formats/testing_sample.hpp
+++ src/formats/testing_sample.hpp
@@ -12,9 +12,9 @@ class TestingSample {
 	static bool image_dims_supported(std::span<const std::size_t> dims);
 
 	static std::optional<std::vector<img::LocalizedImage>>
-
 	load_image(const std::filesystem::path&,
 	           const option_types::options_t& options);
+
 	template <typename T>
 	    requires mt::traits::is_any_of_tuple_v<T,
 	                                           TestingSample::supported_types>
