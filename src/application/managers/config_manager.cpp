d8ce505882e0ab36c3fd8c9ab68b6dd79b7d5b65
Jan Juracka <janjuracka861@seznam.cz>
2023-07-10 17:27:19 +0200

    ADD: inline configs

--- src/application/managers/config_manager.cpp
+++ src/application/managers/config_manager.cpp
@@ -1,15 +1,27 @@
 #include "config_manager.hpp"
 #include <istream>
 
+#ifdef INLINE_CONFIGS
+#include "inlined_configs.hpp"
+#endif
+
 namespace fs = std::filesystem;
 
 namespace ssimp {
 boost::json::value ConfigManager::load_format(const std::string& format) const {
+#ifdef INLINE_CONFIGS
+	return boost::json::parse(ssimp::inlined_configs::formats.at(format));
+#else
 	return _load_json(_format_folder / (format + ".json"));
+#endif
 }
 boost::json::value
 ConfigManager::load_algorithm(const std::string& algorithm) const {
+#ifdef INLINE_CONFIGS
+	return boost::json::parse(ssimp::inlined_configs::algorithms.at(algorithm));
+#else
 	return _load_json(_algo_folder / (algorithm + ".json"));
+#endif
 }
 
 boost::json::value ConfigManager::_load_json(const fs::path& file) const {
