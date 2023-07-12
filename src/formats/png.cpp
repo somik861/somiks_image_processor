888156e2a445dc696def088ae038483771ff7d45
Jan Juracka <janjuracka861@seznam.cz>
2023-07-12 19:08:24 +0200

    ADD: Saving with different colorspace

--- src/formats/png.cpp
+++ src/formats/png.cpp
@@ -5,49 +5,49 @@
 #include <zlib.h>
 
 namespace {
-std::string format_to_string(png_uint_32 format) {
-	switch (format) {
-	case PNG_FORMAT_GRAY:
-		return "GRAY";
-	case PNG_FORMAT_GA:
-		return "GRAY (with alpha)";
-	case PNG_FORMAT_AG:
-		return "GRAY (with alpha first)";
-	case PNG_FORMAT_RGB:
-		return "RGB";
-	case PNG_FORMAT_BGR:
-		return "BGR";
-	case PNG_FORMAT_RGBA:
-		return "RGBA";
-	case PNG_FORMAT_ARGB:
-		return "ARGN";
-	case PNG_FORMAT_BGRA:
-		return "BGRA";
-	case PNG_FORMAT_ABGR:
-		return "ABGR";
-	case PNG_FORMAT_LINEAR_Y:
-		return "Linear GRAY";
-	case PNG_FORMAT_LINEAR_Y_ALPHA:
-		return "Linear GRAY (with alpha)";
-	case PNG_FORMAT_LINEAR_RGB:
-		return "Linear RGB";
-	case PNG_FORMAT_LINEAR_RGB_ALPHA:
-		return "Linear RGBA";
-	case PNG_FORMAT_RGB_COLORMAP:
-		return "RGB with Colormap";
-	case PNG_FORMAT_BGR_COLORMAP:
-		return "BGR with Colormap";
-	case PNG_FORMAT_RGBA_COLORMAP:
-		return "RGB with Colormap";
-	case PNG_FORMAT_ARGB_COLORMAP:
-		return "ARGB with Colormap";
-	case PNG_FORMAT_BGRA_COLORMAP:
-		return "BGRA with Colormap";
-	case PNG_FORMAT_ABGR_COLORMAP:
-		return "ABGR with Colormap";
-	}
+template <typename key_t, typename value_t>
+std::unordered_map<value_t, key_t>
+_mirror_map(const std::unordered_map<key_t, value_t>& map) {
+	std::unordered_map<value_t, key_t> out;
+	for (const auto& [k, v] : map)
+		out[v] = k;
+	return out;
+}
 
-	return std::format("Unrecognized format ID, ID: {}", format);
+std::unordered_map<png_uint_32, std::string> _format_string_map{
+    {PNG_FORMAT_GRAY, "GRAY"},
+    {PNG_FORMAT_GA, "GRAY (with alpha)"},
+    {PNG_FORMAT_AG, "GRAY (with alpha first)"},
+    {PNG_FORMAT_RGB, "RGB"},
+    {PNG_FORMAT_BGR, "BGR"},
+    {PNG_FORMAT_RGBA, "RGBA"},
+    {PNG_FORMAT_ARGB, "ARGN"},
+    {PNG_FORMAT_BGRA, "BGRA"},
+    {PNG_FORMAT_ABGR, "ABGR"},
+    {PNG_FORMAT_LINEAR_Y, "Linear GRAY"},
+    {PNG_FORMAT_LINEAR_Y_ALPHA, "Linear GRAY (with alpha)"},
+    {PNG_FORMAT_LINEAR_RGB, "Linear RGB"},
+    {PNG_FORMAT_LINEAR_RGB_ALPHA, "Linear RGBA"},
+    {PNG_FORMAT_RGB_COLORMAP, "RGB with Colormap"},
+    {PNG_FORMAT_BGR_COLORMAP, "BGR with Colormap"},
+    {PNG_FORMAT_RGBA_COLORMAP, "RGB with Colormap"},
+    {PNG_FORMAT_ARGB_COLORMAP, "ARGB with Colormap"},
+    {PNG_FORMAT_BGRA_COLORMAP, "BGRA with Colormap"},
+    {PNG_FORMAT_ABGR_COLORMAP, "ABGR with Colormap"}};
+auto _string_format_map = _mirror_map(_format_string_map);
+
+const std::string& format_to_string(png_uint_32 format) {
+	if (_format_string_map.contains(format))
+		return _format_string_map.at(format);
+	throw ssimp::exceptions::Unsupported(
+	    std::format("Unrecognized format ID, ID: {}", format));
+}
+
+png_uint_32 string_to_format(const std::string& format) {
+	if (_string_format_map.contains(format))
+		return _string_format_map.at(format);
+	throw ssimp::exceptions::Unsupported(
+	    std::format("Format {} is not supported", format));
 }
 } // namespace
 
@@ -175,18 +175,65 @@ template <typename T>
 	image.version = PNG_IMAGE_VERSION;
 	if constexpr (std::is_same_v<T, img::GRAY8>)
 		image.format = PNG_FORMAT_GRAY;
+	if constexpr (std::is_same_v<T, img::GRAY8A>)
+		image.format = PNG_FORMAT_GA;
 	else if constexpr (std::is_same_v<T, img::RGB8>)
 		image.format = PNG_FORMAT_RGB;
 	else if constexpr (std::is_same_v<T, img::RGBA8>)
 		image.format = PNG_FORMAT_RGBA;
-
 	if (std::get<bool>(options.at("fast_save")))
 		image.flags |= PNG_IMAGE_FLAG_FAST;
 
-	if (!png_image_write_to_file(&image, str_path.c_str(), 0,
-	                             img_.span().data(), 0, nullptr))
-		throw ssimp::exceptions::IOError(
-		    std::format("Could not write: {}", str_path));
+	const void* buffer = img_.span().data();
+
+	std::string colorspace = std::get<std::string>(options.at("colorspace"));
+
+	std::vector<std::byte> new_image_buffer;
+	if (colorspace != "auto") {
+		if (PNG_IMAGE_SAMPLE_CHANNELS(image.format) !=
+		    PNG_IMAGE_SAMPLE_CHANNELS(string_to_format(colorspace)))
+			throw ssimp::exceptions::Unsupported(std::format(
+			    "Cannot convert images to different channel size, got: {}, "
+			    "requested: {}",
+			    PNG_IMAGE_SAMPLE_CHANNELS(image.format),
+			    PNG_IMAGE_SAMPLE_CHANNELS(string_to_format(colorspace))));
+
+		png_alloc_size_t mem_size;
+		if (!png_image_write_get_memory_size(image, mem_size, false, buffer, 0,
+		                                     nullptr))
+			throw ssimp::exceptions::IOError(std::format(
+			    "Obtaining memory requirements ended with message: '{}'",
+			    image.message));
+		std::vector<std::byte> mem_image(mem_size);
+		if (!png_image_write_to_memory(&image, mem_image.data(), &mem_size,
+		                               false, buffer, 0, nullptr))
+			throw ssimp::exceptions::IOError(std::format(
+			    "Writing to memory ended with message: '{}'", image.message));
+
+		png_image new_image;
+		std::memset(&new_image, 0, sizeof(png_image));
+		new_image.version = PNG_IMAGE_VERSION;
+		if (!png_image_begin_read_from_memory(&new_image, mem_image.data(),
+		                                      mem_image.size()))
+			throw ssimp::exceptions::IOError(std::format(
+			    "Begin reading of image from memory ended with message: '{}'",
+			    new_image.message));
+
+		new_image.format = string_to_format(colorspace);
+		new_image_buffer.resize(PNG_IMAGE_SIZE(new_image));
+		if (!png_image_finish_read(&new_image, nullptr, new_image_buffer.data(),
+		                           0, nullptr))
+			throw ssimp::exceptions::IOError(std::format(
+			    "Finish reading of image from memory ended with message: '{}'",
+			    new_image.message));
+		buffer = new_image_buffer.data();
+		image.format = new_image.format;
+	}
+
+	if (!png_image_write_to_file(&image, str_path.c_str(), false, buffer, 0,
+	                             nullptr))
+		throw ssimp::exceptions::IOError(std::format(
+		    "Could not write: {}; (message: '{}')", str_path, image.message));
 }
 
 INSTANTIATE_SAVE_TEMPLATE(PNG, img::GRAY8);
