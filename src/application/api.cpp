5c60612cbcc55417b68c96c0ddf948e8e3122394
Jan Juracka <janjuracka861@seznam.cz>
2023-07-24 17:39:19 +0200

    CHG: API version 0.2 -> 0.3-dev

--- src/application/api.cpp
+++ src/application/api.cpp
@@ -43,7 +43,7 @@ API::API()
 	}
 }
 
-std::string API::version() const { return "0.2"; }
+std::string API::version() const { return "0.3-dev"; }
 
 std::vector<img::LocalizedImage>
 API::load_image(const fs::path& path,
