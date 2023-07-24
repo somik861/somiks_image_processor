7a878354efd6d00a9c7c1d2f013c5c0bf5a7f6f2
Jan Juracka <janjuracka861@seznam.cz>
2023-07-24 09:57:36 +0200

    ADD: implementation

---
+++ src/application/managers/license_manager.cpp
@@ -0,0 +1,28 @@
+#include "license_manager.hpp"
+
+namespace fs = std::filesystem;
+#include <fstream>
+#include <sstream>
+
+namespace ssimp {
+LicenseManager::LicenseManager() {
+	for (const auto& entry : fs::directory_iterator(_license_folder))
+		if (entry.path().extension() == ".lic")
+			_licence_names.insert(entry.path().stem().string());
+}
+
+const std::unordered_set<std::string>&
+LicenseManager::available_licenses() const {
+	return _licence_names;
+}
+const std::string& LicenseManager::license(const std::string& name) const {
+	if (!_loaded.contains(name)) {
+		std::ifstream f(_license_folder / (name + ".lic"));
+		std::stringstream ss;
+		ss << f.rdbuf();
+		_loaded[name] = std::move(ss.str());
+	}
+
+	return _loaded.at(name);
+}
+} // namespace ssimp
--- src/application/managers/license_manager.hpp
+++ src/application/managers/license_manager.hpp
@@ -1,10 +1,33 @@
 #pragma once
+#include <boost/dll.hpp>
+#include <filesystem>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 
 namespace ssimp {
 class LicenseManager {
   public:
-	std::unordered_set<std::string> available_licenses;
+	/**
+	 * Custom c'tor for license loading.
+	 */
+	LicenseManager();
+	/**
+	 * Get set of avaiable licenses
+	 */
+	const std::unordered_set<std::string>& available_licenses() const;
+	/**
+	 * Obtain license text
+	 */
+	const std::string& license(const std::string& name) const;
+
+  private:
+	std::unordered_set<std::string> _licence_names;
+	/**
+	 * Cache of loaded licenses in RAM.
+	 */
+	mutable std::unordered_map<std::string, std::string> _loaded;
+	std::filesystem::path _license_folder =
+	    (boost::dll::program_location().parent_path() / "licenses").c_str();
 };
 } // namespace ssimp
