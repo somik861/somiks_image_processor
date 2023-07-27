d3f73acb5940724d3aba0b9c47f8fb7bec3c2812
Jan Juracka <janjuracka861@seznam.cz>
2023-07-27 16:21:49 +0200

    CHG: main as example :D

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -2,22 +2,24 @@
 #include <filesystem>
 #include <format>
 #include <iostream>
+#include <string>
 
 namespace fs = std::filesystem;
+using namespace std::literals;
 
 int main() {
 
 	try {
-
 		ssimp::API api;
+		auto img = api.load_one("C:\\Users\\janju\\Desktop\\lena_gray.png");
+		img = api.apply({img}, "change_type", {{"output_type", "DOUBLE"}})[0];
+		img = api.apply({img}, "fft")[0];
 
-		std::cout << api.get_properties("lenna.jpg");
-
-		auto img = api.load_image("lenna.jpg")[0];
-		img.location = "lenna2.tiff";
-
-		api.save_image({img.image}, ".", "jpeg", {});
-
+		auto res = img.image.as_typed<ssimp::img::COMPLEX_D>();
+		std::cout << "Result: ";
+		for (std::size_t i = 0; i < 5; ++i)
+			std::cout << res(i) << " ";
+		std::cout << '\n';
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << '\n';
 	}
